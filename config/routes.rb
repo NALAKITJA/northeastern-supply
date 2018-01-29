eb77f785657342b5c395acd39b74d62b5e3a969f
NALAKITJA <mpr_nala@hotmail.com>
2018-01-29 14:41:25 +0700

    detial

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   get 'products' => 'home#product', as: :product
   get 'cleaning/machine' => 'product#cleaning_machine',  as: :cleaning_machine
   get 'cleaning/liquid' => 'product#cleaning_liquid',  as: :cleaning_liquid
-  get 'cleaning/equipment' => 'product#cleaning_equipment',  as: :cleaning_equipment
+  get 'cleaning/kitchen' => 'product#cleaning_kitchen',  as: :cleaning_kitchen
+  get 'cleaning/landry' => 'product#clean_landry',  as: :cleaning_landry
+  get 'cleaning/swimming_pool' => 'product#swimming_pool',  as: :swimming_pool
+  get 'cleaning/industry_product' => 'product#industry_product',  as: :industry_product
+
 end
