7ded58f567b6e93f76dab3ba100a7ce6ff217139
NALAKITJA <mpr_nala@hotmail.com>
2018-03-15 09:58:45 +0700

    update link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => "home#index"
+  get '/' => "home#index"
   get 'set_locale' => 'home#set_locale', as: :set_locale
   get 'products' => 'home#product', as: :product
   get 'cleaning/machine' => 'product#cleaning_machine',  as: :cleaning_machine
